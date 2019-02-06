b439977d4e6d03f24a08987b6efa42b8d5fe2544
alXmas <alexsandr.xmas@gmail.com>
2019-02-06 14:19:05 +0400

    Refactoring converter

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,19 +1,14 @@
 require "rss"
 
 module Converter
-  def self.can_call?(options)
-    if options[:reader] == 'rss'
-      '2.0'
-    elsif options[:reader] == 'atom'
-      'atom'
-    end
-  end
-
   def self.call(head, body, version)
     xml = RSS::Maker.make(version) do |maker|
       maker.channel.title = head[0]['title']
       maker.channel.link = head[1]['link']
       maker.channel.description = 'description'
+      maker.channel.id = 'id'
+      maker.channel.updated = Time.now.to_s
+      maker.channel.author = 'author'
       body.each do |elem|
         maker.items.new_item do |item|
           item.title = elem['title']
--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -2,8 +2,6 @@ require_relative '../lib/reader/file_reader'
 require_relative '../lib/reader/url_reader'
 require_relative '../lib/parser/atom_parser'
 require_relative '../lib/parser/rss_parser'
-require_relative '../lib/converter/atom_converter'
-require_relative '../lib/converter/rss_converter'
 require_relative '../lib/handler/reverse'
 require_relative '../lib/handler/sort'
 require_relative 'converter'
@@ -13,7 +11,6 @@ module Dispatcher
   READERS = [FileReader, UrlReader]
   PARSERS = [AtomParser, RssParser]
   HANDLERS = [Sort, Reverse]
-  # CONVERTERS = [AtomConverter, RssConverter]
 
   def self.run(options, data)
     reader = READERS.find{ |reader| reader.can_call?(data) }
@@ -23,8 +20,12 @@ module Dispatcher
     head= parser.head(feed)
     handler = HANDLERS.find{ |handler| handler.can_call?(options) }
     new_body = handler.call(body)
-    # converter = CONVERTERS.find{ |converter| converter.can_call?(options) }
-    version = Converter.can_call?(options)
+    puts options
+    if options[:reader] == 'rss'
+      version = '2.0'
+    elsif options[:reader] == 'atom'
+      version = 'atom'
+    end
     Converter.call(head,new_body,version)
   end
 end
