25a8934f7ee39ff785e9c98b0f63b934e592ea5e
alXmas <alexsandr.xmas@gmail.com>
2019-02-12 13:25:48 +0400

    Refactoring

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,5 @@
 require 'minitest/autorun'
 Dir.glob('lib/**/*.rb') { |f| require(f) }
 require_relative '../test/fixtures/nokogiri/file'
-require_relative '../../test/fixtures/nokogiri/url'
+require_relative '../test/fixtures/nokogiri/url'
 require_relative '../lib/dispatcher'
